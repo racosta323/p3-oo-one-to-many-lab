c85d498b9a16e1d8d00af9692c84ee6796498e5e
Rene <racosta323@gmail.com>
2024-02-23 17:27:58 -0600

    completes pet class

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,31 +1,34 @@
 import ipdb
 
 class Pet:
+
+    all = []
+
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     
     def __init__(self, name, pet_type, owner=None):
         self.name = name
         self.pet_type = pet_type
         self.owner = owner
-
+        Pet.all.append(self)
+   
     @property
     def pet_type(self):
-        ipdb.set_trace()
         return self._pet_type
     
     @pet_type.setter
     def pet_type(self, new_pet):
-        new_pet = self._pet_type
-        # if (pet for pet in Pet.PET_TYPES if new_pet == pet):
-        #     ipdb.set_trace()
-        #     self._pet_type == new_pet
-        # else: 
-        #     ipdb.set_trace()
-        #     raise Exception
+        # self._pet_type = new_pet
+        pet = [pet for pet in Pet.PET_TYPES if new_pet == pet]
+        if pet:
+            self._pet_type = new_pet
+        else:
+            raise Exception
+            
 
 class Owner:
     def __init__(self, name):
         self.name = name
 
-pet = Pet("Fido", "human")
+# pet = Pet("Fido", "human")
 # ipdb.set_trace()
